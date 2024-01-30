90e85bd729603bcf0bdecf8c850575e4f9ef9ad6
mack lee <114943402+macklee251@users.noreply.github.com>
2024-01-30 20:42:19 -0300

    feat: First interaction

---
+++ src/infra/swapi_api_consumer_test.py
@@ -0,0 +1,7 @@
+from .swapi_api_consumer import SwapiApiConsumer
+
+def test_get_starship():
+    swapi_api_consumer = SwapiApiConsumer()
+    response = swapi_api_consumer.get_starships(page=1)
+    
+    print(response)
